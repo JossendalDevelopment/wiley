e8c0ad32905a58d03e3dd7c0d60b6df0bff78977
Balintataw J <balintataw2@gmail.com>
2019-05-04 17:55:39 -0700

    Set up Jest and vue test utils, simple tests are working

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,43 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+import SignIn from '@/views/SignIn.vue';
+import Home from '@/views/Home.vue';
+
+describe('SignIn.vue renders', () => {
+    let wrp;
+
+    beforeEach(() => {
+        Vue.use(Vuetify);
+    
+        wrp = shallowMount(SignIn, { });
+    });
+
+    // checks SignIn.vue is a component.
+    it('Registers a component', () => {
+        expect(wrp.isVueInstance()).toBeTruthy()
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 })
+describe('Home.vue renders', () => {
+    let wrp;
+
+    beforeEach(() => {
+        Vue.use(Vuetify);
+    
+        wrp = shallowMount(Home, { });
+    });
+    // checks Home.vue is a component.
+    it('Registers a component', () => {
+      expect(wrp.isVueInstance()).toBeTruthy()
+    })
+})
+// describe('SignIn.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(SignIn, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
