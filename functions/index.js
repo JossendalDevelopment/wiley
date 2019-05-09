18fc38412fa80530384640172c2b24cc808db950
Balintataw J <balintataw2@gmail.com>
2019-05-09 16:00:32 -0700

    pulled express-jwt from functions

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,8 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const ejwt = require('express-jwt');
-const config = require('config');
+// const ejwt = require('express-jwt');
+// const config = require('config');
 
 const PORT = process.env.PORT || 3001;
 
@@ -32,7 +32,8 @@ app.use((req, res, next) => {
 });
 
 app.use('/api', publicRouter)
-app.use('/api', ejwt({secret: config.get('jwt-secret')}), authenticatedRouter)
+app.use('/api', authenticatedRouter)
+// app.use('/api', ejwt({secret: config.get('jwt-secret')}), authenticatedRouter)
 
 app.listen(PORT, () => {
     console.log("Server listening on port " + PORT)
