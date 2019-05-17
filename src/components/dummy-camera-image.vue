9d877fb87359ebe32eea3663c251ceb56e94d3a9
Balintataw J <balintataw2@gmail.com>
2019-05-17 16:31:36 -0700

    Updated nav bar and 2nd pass on dummy slider ui

--- src/components/dummy-camera-image.vue
+++ src/components/dummy-camera-image.vue
@@ -1,25 +1,9 @@
+<notes>
+    This component contains the dummy video control bar as well.
+</notes>
 <template>
     <span>
         <v-img :src="getPath()" />
-        <v-layout class="controls" align-center>
-            <!-- Custom video controls UI -->
-            <v-flex grow class="slider">
-                <p class="px-2 mb-0 secondary--text">
-                    Slider
-                </p>
-            </v-flex>
-            <v-flex shrink class="review">
-                <p class="px-2 mb-0 secondary--text">
-                    Review Playback
-                </p>
-            </v-flex>
-            <v-flex shrink class="live">
-                <div class="live-icon"></div>
-                <p class="px-2 mb-0 secondary--text">
-                    Live Feed
-                </p>
-            </v-flex>
-        </v-layout>
     </span>
 </template>
 <script>
@@ -38,34 +22,3 @@ export default {
     },
 }
 </script>
-<style scoped>
-.controls {
-    background-color: var(--v-primary-base);
-    border: 1px solid var(--v-secondary-base);
-    /* position: absolute;
-    bottom: -48px;
-    right: 0px;
-    left: 0px; */
-}
-.live-icon {
-    border: 1px solid;
-    border-radius: 100%;
-    height: 20px;
-    width: 20px;
-    background-color: red;
-}
-.slider {
-    background-color: var(--v-secondary-base);
-    height: 41px;
-}
-.live, .review {
-    display: flex;
-    align-items:center; 
-    padding:5px;
-    margin: 5px 10px;
-}
-.review {
-    background-color: var(--v-primary-darken2);
-}
-</style>
-
