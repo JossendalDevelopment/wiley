e39bde7d655a0cb87ec1b89820fcb5e4fca6f3ed
Balintataw J <balintataw2@gmail.com>
2019-06-25 11:06:18 -0700

    Added edit modal to default history list items, ui only

--- src/components/history--default-list.vue
+++ src/components/history--default-list.vue
@@ -1,3 +1,8 @@
+<notes>
+    This component currently handles lists for all history/archive items except for
+    false alarm types, which need to display a reason for marking as false alarm. These two will
+    likey be merged in the future. They both make use of a default `app-list-item` and slots. 
+</notes>
 <template>
   <v-container grid-list-xs>
     <v-layout justify-center v-if="data.events.length === 0">
@@ -24,19 +29,95 @@
             slot="list-info-bottom-left"
             class="app-list-item-username"
           >{{ $auth.user.email.toUpperCase() }}</span>
-          <v-btn flat small slot="list-info-bottom-right" class="app-list-item-button">EDIT</v-btn>
+          <v-btn
+            flat
+            small
+            slot="list-info-bottom-right"
+            class="app-list-item-button"
+            @click="openEditModal(evt)"
+          >EDIT</v-btn>
         </app-list-item>
       </v-flex>
     </v-layout>
+    <!-- EDIT modal  -->
+    <app-dialog
+      ref="editmodal"
+      max-width="500"
+      lazy
+      v-on:closed="() => null"
+      v-on:opened="() => null"
+    >
+      <template slot="modaltitle">CHANGE CLASSIFICATION</template>
+      <template slot="modalcontent">
+        <v-layout class="modal-content">
+          <v-flex xs4 ma-3 mt-4>
+            <v-img
+              slot="list-image"
+              contain
+              :aspect-ratio="1/1"
+              class="edit-modal-image"
+              :src="selectedForEdit && selectedForEdit.staticImageThumb"
+            ></v-img>
+          </v-flex>
+          <v-flex xs8 pa-3>
+            <v-btn
+              block
+              flat
+              class="classification-btn"
+              @click="() => setNewClass('employee')"
+              :style="newClass === 'employee' ? selectedClass : ''"
+            >employee</v-btn>
+            <v-btn
+              block
+              flat
+              class="classification-btn"
+              @click="() => setNewClass('contractor')"
+              :style="newClass === 'contractor' ? selectedClass : ''"
+            >contractor</v-btn>
+            <v-btn
+              block
+              flat
+              class="classification-btn"
+              @click="() => setNewClass('intruder')"
+              :style="newClass === 'intruder' ? selectedClass : ''"
+            >intruder</v-btn>
+            <v-btn
+              block
+              flat
+              class="classification-btn"
+              @click="() => setNewClass('animal')"
+              :style="newClass === 'animal' ? selectedClass : ''"
+            >animal</v-btn>
+            <v-btn
+              block
+              flat
+              class="classification-btn"
+              @click="() => openFalseAlarmModal()"
+              :style="newClass === 'falsealarm' ? selectedClass : ''"
+            >false alarm</v-btn>
+          </v-flex>
+        </v-layout>
+      </template>
+      <v-btn
+        slot="detailsButton"
+        dark
+        style="background-color:#FFF; color:black;"
+        :disabled="false"
+        @click="saveEdits()"
+      >Confirm</v-btn>
+    </app-dialog>
   </v-container>
 </template>
 <script>
-import appListItem from "@/components/app-list-item.vue";
+import AppListItem from "@/components/app-list-item.vue";
+import AppDialog from "@/components/app-dialog.vue";
+
 import format from "date-fns/format";
 
 export default {
   components: {
-    "app-list-item": appListItem
+    "app-list-item": AppListItem,
+    "app-dialog": AppDialog
   },
   props: {
     data: {
@@ -44,9 +125,32 @@ export default {
       required: true
     }
   },
+  data: () => ({
+    selectedForEdit: null,
+    newClass: null
+  }),
+  computed: {
+    selectedClass() {
+      return "color: var(--v-buttonBlack-base); background-color: #FFF;";
+    }
+  },
   methods: {
     getDateTime(evt) {
       return format(new Date(evt.timestamp._seconds), "MMM DD HH:MM:SSs");
+    },
+    setNewClass(type) {
+      this.newClass = type;
+    },
+    openEditModal(evt) {
+      this.selectedForEdit = evt;
+      this.$refs.editmodal.open();
+    },
+    openFalseAlarmModal() {
+      this.newClass = "falsealarm";
+      // TODO bring in false alarm modal from details.vue
+    },
+    saveEdits() {
+      console.log("NEW CLASSIFICATION", this.newClass);
     }
   }
 };
@@ -72,5 +176,17 @@ export default {
     padding-right: 4px;
     margin: 0;
   }
+}
+.modal-content {
+  border: 1px solid var(--v-border-base);
+  background-color: var(--v-buttonBlack-base);
+}
+.edit-modal-image {
+  //   margin: 10px;
+}
+.classification-btn {
+  border: 1px solid var(--v-border-base);
+  background-color: var(--v-buttonBlack-base);
+  color: #fff;
 }
 </style>
