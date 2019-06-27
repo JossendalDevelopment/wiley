62a0c10d988978e5806bfad6988218ed974b4428
Balintataw J <balintataw2@gmail.com>
2019-06-26 18:05:13 -0700

    Created 404 page, redirect, and route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import firebase from 'firebase';
 
 // import CameraDetailsPage from '@/views/LiveFeed--Details.vue';
 import CameraDetailsPage from '@/views/Details.vue';
+import NotFound from '@/views/NotFound.vue';
 import Overview from '@/views/Overview.vue';
 import History from '@/views/History.vue';
 import SignIn from '@/views/SignIn.vue';
@@ -15,47 +16,61 @@ import EventsJson from './dummyEvents.json';
 Vue.use(Router);
 
 export const router = new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    // { path: '/', name: 'home', component: Home, meta: {
-    //         requiresAuth: true
-    //     } 
-    // },
-    { path: '/sign_in', name: 'signIn', component: SignIn },
-    { path: '/overview', name: 'overview', component: Overview, meta: {
-            requiresAuth: true
-        } 
-    },
-    { path: '/training', name: 'training', component: CameraDetailsPage, meta: {
-            requiresAuth: true
-        }
-    },
-    // { path: '/cam_details/:id', name: 'cam_details', component: CameraDetailsPage, meta: {
-    //         requiresAuth: true
-    //     } 
-    // },
-    { path: '/history', name: 'history', component: History, meta: {
-            requiresAuth: true
-        }
-    },
-    { path: '*', redirect: '/overview' } // this should ideally redirect to / or a 404 page
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        // { path: '/', name: 'home', component: Home, meta: {
+        //         requiresAuth: true
+        //     }
+        // },
+        { path: '/sign_in', name: 'signIn', component: SignIn },
+        {
+            path: '/overview',
+            name: 'overview',
+            component: Overview,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+        {
+            path: '/training',
+            name: 'training',
+            component: CameraDetailsPage,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+        // { path: '/cam_details/:id', name: 'cam_details', component: CameraDetailsPage, meta: {
+        //         requiresAuth: true
+        //     }
+        // },
+        {
+            path: '/history',
+            name: 'history',
+            component: History,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+        { path: '/not_found', name: 'not-found', component: NotFound },
+        { path: '*', redirect: '/not_found' },
+    ],
 });
 
-router.beforeEach( async (to, from, next) => {
+router.beforeEach(async (to, from, next) => {
     // a url based method to reset dummy data for purpose of demo
-    if(to.redirectedFrom && to.redirectedFrom.includes('clear')) {
-        let events = EventsJson.events
+    if (to.redirectedFrom && to.redirectedFrom.includes('clear')) {
+        let events = EventsJson.events;
         try {
-            await store.dispatch('eventHistory/deleteEvents', { 
-                events: events 
-            })
+            await store.dispatch('eventHistory/deleteEvents', {
+                events: events,
+            });
             let resp = await store.dispatch('eventHistory/getAllEvents');
-            await store.dispatch('eventHistory/setEvents', { events: resp.data });
-            next('/training')
-        }
-        catch (error) {
+            await store.dispatch('eventHistory/setEvents', {
+                events: resp.data,
+            });
+            next('/training');
+        } catch (error) {
             throw new Error(error);
         }
         return;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fill-height fluid class="container">
+    <v-layout align-center justify-center>
+      <v-flex xs4 class="box">
+        <h1 class="numeral text-xs-center">404</h1>
+        <h3 class="text text-xs-center">LOOKS LIKE SOMETHING WENT WRONG</h3>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<style lang="scss" scoped>
+.container {
+  //   background-color: var(--v-secondaryDark-base);
+  font-family: "DIN Condensed";
+  letter-spacing: 1.5px;
+  .box {
+    transform: translateY(-80px);
+  }
+  .numeral {
+    font-size: 18rem;
+    color: #fff;
+    letter-spacing: 10px;
+    margin-bottom: -70px;
+  }
+  .text {
+    color: var(--v-border-base);
+    font-size: 3rem;
+    width: 90%;
+    margin: 0 auto;
+  }
+}
+</style>
+
