7580f805df2d0b478ec77849d48f90cfb0e5f507
Balintataw J <balintataw2@gmail.com>
2019-06-26 18:23:18 -0700

    Fixed bug: invalid date on false alarm archive screen

--- src/components/history--false-alarm-list.vue
+++ src/components/history--false-alarm-list.vue
@@ -1,31 +1,31 @@
 <template>
-    <v-container>
-        <v-layout justify-center v-if="data.events.length === 0">
-            <span style="color:white; font-family: DIN Condensed; font-size: 30px;">
-            THERE ARE NO EVENTS FOR {{ data.type.toUpperCase() }}S
-            </span>
-        </v-layout>
-        <v-layout v-else row wrap justify-center>
-            <v-flex xs12 v-for="(evt) in data.events" :key="evt.eventId">
-                <app-list-item>
-                    <v-img
-                        slot="list-image"
-                        contain
-                        :aspect-ratio="1/1"
-                        class="app-list-item-image"
-                        :src="evt.staticImageThumb"
-                    ></v-img> 
-                    <span slot="list-info-top-left"
-                          class="app-list-item-username">{{ $auth.user.email }}</span>
-                    <span slot="list-info-top-right"
-                          class="app-list-item-date">{{ getDate(evt) }}</span>
-                    <span slot="list-info-bottom-left" 
-                          class="app-list-item-content"
-                          v-trim="90">{{ evt.confirmationDescription }}</span>
-                    <!-- <span slot="list-info-bottom-right"  -->
-                          <!-- class="app-list-item-date">{{ getDate(evt) }}</span> -->
-                </app-list-item>
-                <!-- <v-card flat class="app-list-item">
+  <v-container>
+    <v-layout justify-center v-if="data.events.length === 0">
+      <span
+        style="color:white; font-family: DIN Condensed; font-size: 30px;"
+      >THERE ARE NO EVENTS FOR {{ data.type.toUpperCase() }}S</span>
+    </v-layout>
+    <v-layout v-else row wrap justify-center>
+      <v-flex xs12 v-for="(evt) in data.events" :key="evt.eventId">
+        <app-list-item>
+          <v-img
+            slot="list-image"
+            contain
+            :aspect-ratio="1/1"
+            class="app-list-item-image"
+            :src="evt.staticImageThumb"
+          ></v-img>
+          <span slot="list-info-top-left" class="app-list-item-username">{{ $auth.user.email }}</span>
+          <span slot="list-info-top-right" class="app-list-item-date">{{ getDate(evt) }}</span>
+          <span
+            slot="list-info-bottom-left"
+            class="app-list-item-content"
+            v-trim="90"
+          >{{ evt.confirmationDescription }}</span>
+          <!-- <span slot="list-info-bottom-right"  -->
+          <!-- class="app-list-item-date">{{ getDate(evt) }}</span> -->
+        </app-list-item>
+        <!-- <v-card flat class="app-list-item">
                     <v-layout>
                         <v-flex xs2 class="app-list-item-image-container">
                             <v-img
@@ -48,47 +48,47 @@
 
 
                     </v-layout>
-                </v-card> -->
-            </v-flex>
-        </v-layout>
-    </v-container>
+        </v-card>-->
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
-import appListItem from '@/components/app-list-item.vue';
-import format from 'date-fns/format';
+import appListItem from "@/components/app-list-item.vue";
+import format from "date-fns/format";
 
 export default {
-    components: {
-        'app-list-item': appListItem,
-    },
-    props: {
-        data: {
-            type: Object,
-            required: true
-        },
-    },
-    methods: {
-        getDate(evt) {
-            return format(new Date(evt.timestamp._seconds), 'MMM DD');
-        },
-    },
-}
+  components: {
+    "app-list-item": appListItem
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getDate(evt) {
+      return format(new Date(evt.timestamp.seconds), "MMM DD");
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .app-list-item {
-    &-image {
-        margin: 0 auto;
-    }
-    &-username {
-        margin-bottom: 0px;
-    }
-    &-date {
-        color: var(--v-border-base);
-        margin-bottom: 0px;
-    }
-    &-content {
-        padding-top: 0px;
-        font-size: 16px;
-    }
+  &-image {
+    margin: 0 auto;
+  }
+  &-username {
+    margin-bottom: 0px;
+  }
+  &-date {
+    color: var(--v-border-base);
+    margin-bottom: 0px;
+  }
+  &-content {
+    padding-top: 0px;
+    font-size: 16px;
+  }
 }
 </style>
