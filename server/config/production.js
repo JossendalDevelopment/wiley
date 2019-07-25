405e7d35de756c8d84f166f1b1389224adea0489
Balintataw J <balintataw2@gmail.com>
2019-07-24 17:23:50 -0700

    Removed dead config file in /server

--- server/config/production.js
+++
@@ -1,9 +0,0 @@
-// this will set the path of where the api server will look in the 
-// fileserver to find the correct metadata.json file/s
-// RESULTING PATH: <image_filepath>/<YEAR>/<MONTH>/<DAY>/metadata.json
-module.exports = {
-    // used in server/metaDataFile.js
-    image_host: 'localhost',
-    image_filepath: '/mnt/sol2_video/east-inferenced/raw',
-    port: 3000,
-};
