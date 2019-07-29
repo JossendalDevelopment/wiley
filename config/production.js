56474cdb2a60b08b7a87ad9aeb902317ae6e8253
Balintataw J <balintataw2@gmail.com>
2019-07-29 15:41:25 -0700

    Update gitignore

--- config/production.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // used in views/overview.vue
-    stream_data_one: 'http://localhost:3000/live/streams/one/480p.m3u8',
-    stream_data_two: 'http://localhost:3000/live/streams/two/480p.m3u8',
-};
