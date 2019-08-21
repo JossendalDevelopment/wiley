ac2eb4be9c192e6d06ff858cc9296d701b84d239
Balintataw J <balintataw2@gmail.com>
2019-08-21 11:51:41 -0700

    Setting modified date to iso format on an alert when it is received by the app

--- server/routes/public-router.js
+++ server/routes/public-router.js
@@ -12,6 +12,8 @@ router.get('/', (req, res) => {
 
 router.post('/trigger_alarm', (req, res) => {
     const data = req.body
+    data.modified_date = new Date().toISOString();
+
     req.io.sockets.emit('TRIGGER_ALARM', data);
 
     db.one('INSERT INTO alerts(bbox_height, bbox_width, bbox_xmin, bbox_ymin, camera, classification_description, classified_by, id, image_filename, image_filepath, image_height, image_width, inferenced_classification, inferenced_percentage, modified_date, thumb_filename, thumb_filepath, thumb_250x250, thumb_height, thumb_width, user_classification, video_clip_filepath, video_clip_name) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23) RETURNING id', Object.values(new Event(data)))
